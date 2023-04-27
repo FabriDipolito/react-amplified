056078bc84ca444543e4fa32a6fdb3a25ebc9a67
FabriDipolito <fdipolito@effectussoftware.com>
2023-04-27 16:37:05 -0300

    project mock-up

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "ticvi": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
