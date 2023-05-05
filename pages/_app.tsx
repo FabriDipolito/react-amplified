42609631776e3f4c588a6161c2065e636fd5cba2
FabriDipolito <fdipolito@effectussoftware.com>
2023-05-04 21:55:49 -0300

    deploy2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
-import awsExports from "../aws-exports";
+import awsExports from "../aws-exports.js";
 import { Button } from "@aws-amplify/ui-react";
 import type { AppProps } from "next/app";
 
